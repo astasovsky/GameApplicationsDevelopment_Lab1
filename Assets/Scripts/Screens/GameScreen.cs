d73e8acae8d7ec7cc8f3ce75807fad66f54acdf7
astasovsky <stasovsky.ua@gmail.com>
2023-04-22 11:51:43 +0300

    Task 2. Done

---
+++ Assets/Scripts/Screens/GameScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Screens
+{
+    public class GameScreen : MonoBehaviour
+    {
+        [SerializeField] private Button mainMenuButton;
+
+        private void Awake()
+        {
+            mainMenuButton.onClick.AddListener(delegate { SceneManager.LoadScene(SceneName.MainScene.ToString()); });
+        }
+    }
+}
---
+++ Assets/Scripts/Screens/MainMenuScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Screens
+{
+    public class MainMenuScreen : MonoBehaviour
+    {
+        [SerializeField] private Button startButton;
+        [SerializeField] private Button settingsButton;
+        [SerializeField] private Button exitButton;
+
+        private void Awake()
+        {
+            startButton.onClick.AddListener(delegate { SceneManager.LoadScene(SceneName.GameScene.ToString()); });
+            settingsButton.onClick.AddListener(delegate { });
+            exitButton.onClick.AddListener(Application.Quit);
+        }
+    }
+}
